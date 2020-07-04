8e0fe62709a5b5ce03458f08a058b9b4c7387bfd
Rajeev Gurram <rgurram@paloaltonetworks.com>
2020-07-03 17:12:03 -0700

    added folder structure

---
+++ src/main/java/com/github/rajeevgurram/kafka/tutorial_01/ProducerDemo.java
@@ -0,0 +1,8 @@
+package com.github.rajeevgurram.kafka.tutorial_01;
+
+// 3 steps to create producer
+public class ProducerDemo {
+    public static void main(String[] args) {
+        // Step1: Create producer properties.
+    }
+}
